cb32862083f3f5705d5961aef43197bc298c598b
enkhee-Osiris <enkhee.ag@gmail.com>
2018-04-30 23:31:00 +0800

    add size for navigation icon

--- src/components/NavIcon.js
+++ src/components/NavIcon.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'react-native-elements';
 
-const NavIcon = ({ tintColor, ...props }) => (
+const NavIcon = ({ tintColor, size = 21, ...props }) => (
   <Icon
     type="ionicon"
     underlayColor="transparent"
     color={tintColor}
+    size={size}
     {...props}
   />
 );
