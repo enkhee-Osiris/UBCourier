a34749b57f5b57124bfd70750d474552b6ceacac
enkhee-Osiris <enkhee.ag@gmail.com>
2018-04-30 14:39:18 +0800

    add auth

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import appReducer from './app';
 import navigatorReducer from './navigator';
 import storageReducer from './storage';
+import authReducer from './auth';
 
 const rootReducer = combineReducers({
   app: appReducer,
   navigator: navigatorReducer,
   storage: storageReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
