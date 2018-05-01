ffcebb717f0677a7a4d845db47a8caaed2409167
enkhee-Osiris <enkhee.ag@gmail.com>
2018-05-01 18:30:55 +0800

    update auth operations

--- src/modules/auth/operations.js
+++ src/modules/auth/operations.js
@@ -1,42 +1,38 @@
-// import { Facebook } from 'expo';
-// import firebase from '../../config/firebase';
+import { Facebook } from 'expo';
+import firebase from '../../config/firebase';
 import { FACEBOOK_APP_ID } from '../../config/local';
 import {
   loggedIn,
   loggedOut,
+  errorOccured,
 } from './actions';
 
 const logIn = (email, password) => async (dispatch) => {
-  try {
-    // await firebase.auth()
-    //   .signInWithEmailAndPassword(email, password)
-    //   .then(user => (dispatch(loggedIn(user))));
-    dispatch(loggedIn({ email: 'enkhee.ag', uid: '123' }));
-  } catch (error) {
-    console.log(error);
-  }
+  await firebase.auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(user => (dispatch(loggedIn(user))))
+    .catch(error => (dispatch(errorOccured(error))));
 };
 
 const logOut = () => async (dispatch) => {
-  // await firebase.auth()
-  //   .signOut()
-  //   .then(() => (dispatch(loggedOut)))
-  //   .catch(error => (console.log(error)));
-  dispatch(loggedOut);
+  await firebase.auth()
+    .signOut()
+    .then(() => (dispatch(loggedOut)))
+    .catch(error => (dispatch(errorOccured(error))));
 };
 
 const logInWithFacebook = () => async (dispatch) => {
-  // const { type, token } = await Facebook
-  //   .logInWithReadPermissionsAsync(FACEBOOK_APP_ID, { permissions: ['public_profile'] });
+  const { type, token } = await Facebook
+    .loginwithreadpermissionsasync(FACEBOOK_APP_ID, { permissions: ['public_profile'] });
 
-  // if (type === 'success') {
-    // const credential = firebase.auth.FacebookAuthProvider.credential(token);
+  if (type === 'success') {
+    const credential = firebase.auth.FacebookAuthProvider.credential(token);
 
-    // await firebase.auth()
-    //   .signInWithCredential(credential)
-    //   .then(user => (dispatch(loggedIn(user))))
-    //   .catch(error => (console.log(error)));
-  // }
+    await firebase.auth()
+      .signInWithCredential(credential)
+      .then(user => (dispatch(loggedIn(user))))
+      .catch(error => (dispatch(errorOccured(error))));
+  }
   dispatch(loggedIn({ email: 'enkhee.ag', uid: '123' }));
 };
 
