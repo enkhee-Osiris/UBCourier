bc350432107f258e040cd797944ecc6983469246
enkhee-Osiris <enkhee.ag@gmail.com>
2018-05-02 11:43:46 +0800

    add header options

---
+++ src/styles/stackHeaderOptions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationButton } from '../components';
+import screens from '../constants/screens';
+
+const headerOptions = () => ({ navigation }) => {
+  return {
+    headerLeft: <NavigationButton
+      iconName="ios-menu-outline"
+      onPress={() => navigation.navigate(screens.DrawerOpen)}
+    />,
+  };
+};
+
+export default headerOptions;
