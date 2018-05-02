15f4a056dcf72634441017ddd9b693bcad8aa103
enkhee-Osiris <enkhee.ag@gmail.com>
2018-05-02 11:43:17 +0800

    add menu button on home screen

--- src/navigation/navigators/HomeNavigator.js
+++ src/navigation/navigators/HomeNavigator.js
@@ -2,10 +2,12 @@ import { StackNavigator } from 'react-navigation';
 import { Home } from '../../screens';
 import screens from '../../constants/screens';
 import navOptions from '../../styles/navOptions';
+import headerOptions from '../../styles/stackHeaderOptions';
 
 const HomeNavigator = StackNavigator({
   [screens.Home]: {
     screen: Home,
+    navigationOptions: headerOptions(),
   },
 }, {
   ...navOptions({
