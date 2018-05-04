d74c2495b3155ca97e347bed15dbb083a2f72286
enkhee-Osiris <enkhee.ag@gmail.com>
2018-05-04 23:22:07 +0800

    add user default avatar

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+export const defaultUserAvatar = 'https://firebasestorage.googleapis.com/v0/b/ubcourier-001.appspot.com/o/default_avatar.png?alt=media&token=3d59ed33-6630-4ff3-85b5-4ce355bd23f7';
 
 export default [
   require('../assets/icons/splash.png'),
