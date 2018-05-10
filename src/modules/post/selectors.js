db5215f59a0ca0833ae3e45cfe740f1f5feff3ca
enkhee-Osiris <enkhee.ag@gmail.com>
2018-05-10 14:31:04 +0800

    add selectors for post

---
+++ src/modules/post/selectors.js
@@ -0,0 +1,58 @@
+import { createSelector } from 'reselect';
+
+const getPostsIds = posts => posts.ids || [];
+const getPostsEntities = posts => posts.byId || {};
+const getUidForFiltering = uid => uid;
+
+export const getUserPosts = createSelector(
+  [
+    getPostsIds,
+    getPostsEntities,
+    getUidForFiltering,
+  ],
+  (ids, entities, uid) => {
+    const posts = [];
+    ids.forEach((id) => {
+      if (entities[id].userId === uid) {
+        posts.push(entities[id]);
+      }
+    });
+
+    return posts;
+  },
+);
+
+export const getLocations = createSelector(
+  [
+    getPostsIds,
+    getPostsEntities,
+  ],
+  (ids, entities) => {
+    const newArray = [];
+    ids.forEach((id) => {
+      const location = entities[id].currentLocation;
+      newArray.push(location);
+    });
+
+    return newArray;
+  },
+);
+
+export const getUserTotalBalance = createSelector(
+  [
+    getPostsIds,
+    getPostsEntities,
+    getUidForFiltering,
+  ],
+  (ids, entities, uid) => {
+    let totalPrice = 0;
+    ids.forEach((id) => {
+      const post = entities[id];
+      if (post.userId === uid) {
+        totalPrice += post.price;
+      }
+    });
+
+    return totalPrice;
+  },
+);
